87035733ee99b0ec70267d981c12060fb632c65f
Winston <winston@nitidbit.com>
2024-10-16 10:02:51 -0700

    test server

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,17 @@ import app_json from './App.json'
 
 function App() {
   const lexicon = useLexicon(app_json)
+  const product_name:string = lexicon.get('product_name')
   return (
     <div className="App">
-      <LxProvider apiUpdateUrl="http://example.com/update">
+      <LxProvider apiUpdateUrl="https://lexicon-staging.nitid.co/update">
         <div>
           <a href="https://thenounproject.com/creator/joerivera2000/">
             <img className="logo" src={ostrich} />
           </a>
         </div>
         <h1> { lexicon.get('title') } </h1>
-        <p> { lexicon.get('blurb', {product_name: lexicon.get('product_name')} ) } </p>
+        <p> { lexicon.get('blurb', {product_name} ) } </p>
       </LxProvider>
     </div>
   )
